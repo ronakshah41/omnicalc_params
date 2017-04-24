9cd4f2f9691a2185707022ae15079c040e5c6563
Ronak Shah <ronakshah41@users.noreply.github.com>
2017-04-24 11:41:38 -0500

    /first draft

---
+++ app/controllers/calculations_controller.rb
@@ -0,0 +1,88 @@
+class CalculationsController <ApplicationController
+
+  def flex_square_5
+    @user_number = params["num"].to_f
+    @square = @user_number**2
+    render("/calculations/flex_square_5.html.erb")
+  end
+
+  def flex_squareroot
+    @user_number = params[:num].to_f
+    @squareroot = @user_number** 0.5
+    render("/calculations/flex_square_root.html.erb")
+  end
+
+  def flex_random
+    @min = params[:min].to_i
+    @max = params[:max].to_i
+    @random = (@min..@max).to_a.sample
+    render("/calculations/flex_random.html.erb")
+  end
+
+  def flex_payment
+    @rate = params[:rate].to_f
+    @year = params[:year].to_i
+    @principal = params[:principal].to_f
+
+    @annual_r = @rate / 100.0
+    monthly_r = @annual_r / 12.0 / 100.00
+    terms = @year * 12
+    numerator = monthly_r * (@principal)
+    denominator = 1 - ((1+monthly_r)**-terms)
+
+
+  @monthly_payment = numerator / denominator
+
+    render("/calculations/flex_payment.html.erb")
+  end
+
+  def square_form
+    render("/calculations/square_form.html.erb")
+  end
+
+  def square
+    @user_number = params[:user_number].to_i
+    @square = @user_number** 2
+    render("/calculations/square.html.erb")
+  end
+
+  def square_root_form
+    render("/calculations/square_root_form.html.erb")
+  end
+
+  def square_root
+    @user_number = params[:user_number].to_i
+    @square = @user_number** 0.5
+    render("/calculations/square_root.html.erb")
+  end
+
+  def random_form
+    render("/calculations/random_form.html.erb")
+  end
+
+  def random
+    @min = params[:min].to_i
+    @max = params[:max].to_i
+    @random = (@min..@max).to_a.sample
+    render("/calculations/random.html.erb")
+  end
+
+  def payment_form
+    render("/calculations/payment_form.html.erb")
+  end
+
+  def payment
+    @rate = params[:rate].to_f
+    @year = params[:year].to_i
+    @principal = params[:principal].to_f
+
+    monthly_r = @rate / 12.0 / 100.0
+    terms = @year * 12
+    numerator = monthly_r * (@principal)
+    denominator = 1 - ((1+monthly_r)**-terms)
+
+
+  @monthly_payment = numerator / denominator
+    render("/calculations/payment.html.erb")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get("/flexible/square/:num", {:controller=> "calculations", :action => "flex_square_5"})
+
+get("/flexible/square_root/:num", {:controller=> "calculations", :action => "flex_squareroot"})
+
+get("/flexible/random/:min/:max", {:controller=> "calculations", :action => "flex_random"})
+
+get("/flexible/payment/:rate/:year/:principal", {:controller=> "calculations", :action => "flex_payment"})
+
+get("/square/new", {:controller => "calculations", :action => "square_form"})
+
+get("/square/results", {:controller => "calculations", :action => "square"})
+
+get("/square_root/new", {:controller => "calculations", :action => "square_root_form"})
+
+get("/square_root/results", {:controller => "calculations", :action => "square_root"})
+
+get("/random/new", {:controller => "calculations", :action => "random_form"})
+
+get("/random/results", {:controller => "calculations", :action => "random"})
+
+get("/payment/new", {:controller => "calculations", :action => "payment_form"})
+
+get("/payment/results", {:controller => "calculations", :action => "payment"})
 end
